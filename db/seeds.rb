b7276235f95807fa97145c46a74751550c948723
sutairusu <sutairusu@icloud.com>
2023-02-20 18:15:10 +0900

    Seed database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(
+  first_name: "John",
+  last_name: "Baker",
+  email: "john.baker@gmail.com",
+  address: "123 Main Street",
+  password: 123123
+)
+
+User.create(
+  first_name: "Keina",
+  last_name: "Morikawa",
+  email: "keina.morikawa@me.com",
+  address: "1-2-45 Chuomachi",
+  password: 123123
+)
+
+20.times do
+  Equipment.create!(
+    model: Faker::Camera.model,
+    brand: Faker::Camera.brand,
+    description: Faker::Lorem.sentences(number: 3),
+    price: 2000,
+    user: User.all.sample
+  )
+end
