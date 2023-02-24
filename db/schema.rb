9609263d4b9afa14cc3b8d65687bdcedebaac5c5
sutairusu <sutairusu@icloud.com>
2023-02-24 12:06:03 +0900

    Update schema so that description is now a text data type instead of string. Also, make a small adjustment to the order of the items in the add camera form.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_23_052043) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_24_024749) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,7 +58,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_23_052043) do
     t.bigint "user_id", null: false
     t.string "model"
     t.string "brand"
-    t.string "description"
+    t.text "description"
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
